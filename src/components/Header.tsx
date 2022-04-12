3d317e8b3f19a33035ce690ee443b8332791fc60
Sloth Today <sloth.today22@gmail.com>
2022-04-12 13:23:11 +0530

    feat: header css fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,27 +21,35 @@ const Header = () => {
         spacing={2}
         sx={{ py: 3, backgroundColor: `#181522` }}
       >
-        <Grid
-          container
-          item
-          xs={6}
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <Logo />
-          <HeaderTab label="Marketplace" selected />
-          <HeaderTab label="Dashboard" />
-          <HeaderTab label="Governance" />
-        </Grid>
+        <Grid container sx={{ maxWidth: 1400, margin: `auto` }}>
+          <Grid
+            container
+            item
+            xs={5}
+            justifyContent="space-evenly"
+            alignItems="center"
+          >
+            <Logo />
+            <HeaderTab label="Marketplace" selected />
+            <HeaderTab label="Dashboard" highlighted />
+            <HeaderTab label="Governance" />
+          </Grid>
 
-        <Grid container item xs={6} justifyContent="center">
-          <Button outlined onClick={() => null}>
-            Buy USDC
-          </Button>
-          <Button outlined onClick={() => null}>
-            Network
-          </Button>
-          <Button onClick={onClickConnectWallet}>Connect Wallet</Button>
+          <Grid
+            container
+            item
+            xs={7}
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Button outlined onClick={() => null}>
+              Buy USDC
+            </Button>
+            <Button outlined onClick={() => null}>
+              Network
+            </Button>
+            <Button onClick={onClickConnectWallet}>Connect Wallet</Button>
+          </Grid>
         </Grid>
       </Grid>
       {connectWalletPopup && (
