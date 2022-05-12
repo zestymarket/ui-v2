006678050cc768ae6eef907ca815751a12597a01
Sloth Today <sloth.today22@gmail.com>
2022-05-12 19:19:40 +0200

    fix: image uploader component and refactoring

--- src/pages/create-campaign.tsx
+++ src/pages/create-campaign.tsx
@@ -11,29 +11,23 @@ import {
   FormHelperText,
   styled,
 } from '@mui/material';
-// https://github.com/lifeeric/material-ui-dropzone#installation
-import { DropzoneArea } from 'react-mui-dropzone';
 import validator from 'validator';
 import SubHeader from '@/components/SubHeader';
 import Button from '@/components/Button';
-import { readFile } from '@/utils/file';
 import {
   Format,
   FormatCategories,
   SpaceFormatResolutions,
-  SpaceFormats,
 } from '@/utils/formats';
 import {
   convertBase64ToFile,
-  getAspectFromFormat,
   getHeightFromFormat,
   getWidthFromFormat,
 } from '@/utils/image';
-import { EPSILON } from '@/utils/helpers';
 import { pinFileToIPFS, pinJSONToIPFS } from '@/lib/ipfs';
 import { useZestyMarketUSDC } from '@/utils/hooks';
-import ZestyImageDialog from '@/components//ZestyImageDialog';
 import { useRouter } from 'next/router';
+import ZestyImageUploader from '@/components/ZestyImageUploader';
 
 const StyledForm = styled(Box)({
   maxWidth: 1400,
@@ -74,74 +68,6 @@ const StyleHelperText = styled(FormHelperText)({
   bottom: -20,
   right: 0,
 });
-const StyledDropzoneArea = styled(Stack)<StackProps>(({ theme }) => ({
-  '& .dropzone': {
-    height: 56,
-    width: 540,
-    minHeight: 56,
-    backgroundColor: `transparent`,
-    border: `1px dashed #ffffff30`,
-    display: `flex`,
-    alignItems: `center`,
-    justifyContent: `center`,
-    position: `relative`,
-  },
-  '& .dropzone-paragraph': {
-    color: `#5F5777`,
-    fontWeight: 400,
-    fontSize: 18,
-    margin: 0,
-  },
-  '& .MuiDropzoneAreaActive': {},
-  '& .MuiDropzoneArea-textContainer': {
-    display: `flex`,
-    flexDirection: `row-reverse`,
-  },
-  '& .MuiDropzoneArea-icon': {
-    width: 24,
-    height: 24,
-    color: `#5F5777`,
-    marginRight: 12,
-  },
-  '& .MuiDropzonePreviewList-root': {
-    width: 540,
-    position: `absolute`,
-    textAlign: `left`,
-    justifyContent: `center`,
-    backgroundColor: theme.palette.background.paper,
-    margin: 0,
-  },
-  '& .MuiDropzonePreviewList-image': {
-    display: `none`,
-  },
-  '& .MuiDropzonePreviewList-imageContainer': {
-    display: `flex`,
-    alignContent: `center`,
-    padding: `0 16px !important`,
-    backgroundColor: `#413859`,
-    height: 56,
-    width: 540,
-    maxWidth: `100%`,
-    textAlign: `left`,
-
-    '& p': {
-      lineHeight: `54px`,
-      width: 540,
-    },
-  },
-  '& .MuiDropzonePreviewList-removeButton': {
-    backgroundColor: `transparent`,
-    boxShadow: `none`,
-    color: theme.palette.text.secondary,
-    opacity: 1,
-    top: 8,
-    right: 8,
-    '&:hover': {
-      backgroundColor: `transparent`,
-      color: theme.palette.text.primary,
-    },
-  },
-}));
 
 const formFieldProps: StackProps = {
   direction: `row`,
@@ -171,17 +97,13 @@ const getFormatChoices = () => {
 
 const CreateCampaign = () => {
   const zestyMarketUSDC = useZestyMarketUSDC(true);
-
   const router = useRouter();
 
   const [name, setName] = useState<string>(``);
   const [format, setFormat] = useState<string>(``);
-  const [dropzoneKey, setDropzoneKey] = useState<string>(`0`);
   const [image, setImage] = useState<string | null>(null);
   const [url, setURL] = useState<string>(``);
   const [description, setDescription] = useState<string>(``);
-
-  const [openImageDialog, setOpenImageDialog] = useState<boolean>(false);
 
   const isSubmitDisabled =
     !name ||
@@ -189,54 +111,6 @@ const CreateCampaign = () => {
     image === null ||
     !url ||
     (url && !validator.isURL(url));
-
-  const onImageUploaded = async (img: File[]) => {
-    if (!img.length) {
-      return;
-    }
-    const file = img[0];
-    const imageDataUrl = await readFile(file);
-    if (format == SpaceFormats.Twitch && file.type === `image/gif`) {
-      const img = new Image();
-      img.onload = () => {
-        const twitchHeight = getHeightFromFormat(SpaceFormats.Twitch);
-        const twitchAspectRatio = getAspectFromFormat(SpaceFormats.Twitch);
-        const imageAspectRatio = img.width / img.height;
-        // Checks if the image aspects are the same, avoids float rounding issues
-        if (
-          img.height === twitchHeight &&
-          Math.abs(imageAspectRatio - twitchAspectRatio) < EPSILON
-        ) {
-          setImage(imageDataUrl);
-        } else {
-          // error
-          setDropzoneKey(dropzoneKey === `0` ? `1` : `0`);
-        }
-      };
-      img.src = imageDataUrl;
-    } else {
-      setImage(imageDataUrl);
-      setOpenImageDialog(true);
-    }
-  };
-
-  const onImageDelete = () => {
-    setImage(null);
-  };
-
-  const onImageCropped = (img: string): void => {
-    setImage(img);
-  };
-
-  const onCloseDialog = (e: any) => {
-    setOpenImageDialog(false);
-    // Remove image if dialog was closed due to an event and
-    // not from a function call with false
-    if (e) {
-      setImage(null);
-      setDropzoneKey(dropzoneKey === `0` ? `1` : `0`);
-    }
-  };
 
   const onSubmit = async () => {
     if (image === null) {
@@ -316,7 +190,7 @@ const CreateCampaign = () => {
           </StyledSelect>
         </Stack>
 
-        <StyledDropzoneArea {...formFieldProps}>
+        <Stack {...formFieldProps}>
           <Stack direction="row" alignItems="baseline">
             <StyledLabel>The Frontpage</StyledLabel>
             {format && (
@@ -325,30 +199,9 @@ const CreateCampaign = () => {
               </StyledSubLabel>
             )}
           </Stack>
-          <DropzoneArea
-            key={dropzoneKey}
-            clearOnUnmount
-            showFileNames
-            showAlerts={false}
-            filesLimit={1}
-            maxFileSize={2097152} // 2MB
-            dropzoneText={`Drag and Drop your files here`}
-            acceptedFiles={[`image/*`]}
-            dropzoneClass="dropzone"
-            dropzoneParagraphClass="dropzone-paragraph"
-            onChange={onImageUploaded}
-            onDelete={onImageDelete}
-          />
-        </StyledDropzoneArea>
-        {image && (
-          <ZestyImageDialog
-            format={format as Format}
-            open={openImageDialog}
-            onDialogClose={onCloseDialog}
-            image={image}
-            onImageCropped={onImageCropped}
-          />
-        )}
+          <ZestyImageUploader format={format} onImageUpdate={setImage} />
+        </Stack>
+
         <Stack {...formFieldProps}>
           <StyledLabel>Call to action URL</StyledLabel>
           <StyledTextField
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import TabsContainer from '@/components/TabsContainer';
 import Overview from './components/Overview';
-import Account from './components/Account';
+import Account from '@/components/dashboard/Account';
+import MySpaces from '@/components/dashboard/MySpaces';
 import StyledDashboard from './styled';
 import MySpaces from './components/MySpaces';
 
@@ -34,6 +35,11 @@ export default function Dashboard() {
             id: `account-management`,
             label: `Account Management`,
             PanelComponent: Account,
+          },
+          {
+            id: `spaces`,
+            label: `My Spaces`,
+            PanelComponent: MySpaces,
           },
         ]}
       />
